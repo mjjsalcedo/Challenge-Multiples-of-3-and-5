ec4e9d960e48128a3cdadf74fb7e891244e84cfd
Mariel Salcedo <Mariel Salcedo>
2017-06-13 11:01:48 -1000

    completed

--- multiples.js
+++ multiples.js
@@ -3,10 +3,30 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
-exports.sumOfAMultiple = function( n ) {
+ var sumOfAMultiple = function( n ) {
   var sum = 0;
-
+  var threesum = [];
+  var fivesum = [];
   // do your work here
 
+  for (i = 0; i <n; i++){
+
+    if (i % 3 === 0) {
+      threesum.push(i);}
+    else if (i % 5 === 0) {
+      fivesum.push(i);}
+    }
+
+    function getSum(total, num) {
+    return total + num;
+    }
+
+  sum = threesum.reduce(getSum) + fivesum.reduce(getSum);
+
   return sum;
+
 };
+
+console.log(sumOfAMultiple(15));
+
+
